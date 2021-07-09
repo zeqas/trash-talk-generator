43b62698afc77a9337e1b9fd783e0b9b2f78b1bc
zeqas <casperi52400@gmail.com>
2021-07-10 00:14:41 +0800

    feat: json renamed; gitignore renewed

--- models/talkShit.js
+++ models/talkShit.js
@@ -1,7 +1,15 @@
-function randomIndex(array) {
-  return Math.floor(Math.random() * array.length)
+const list = require('./shitsData.json')
+
+function sample(array) {
+  const index = Math.floor(Math.random() * array.length)
+  return array[index]
 }
 
-function talkShit (target) {
-  let 
+// define trashTalkGenerator function
+function trashTalkGenerator(target) {
+  if (!target) { return '請選一個角色。' }
+  return `身為一個${list.job[target].title}，${sample(list.task[target])}，${sample(list.phrase)}吧！`
 }
+
+// export trashTalkGenerator
+module.exports = trashTalkGenerator
