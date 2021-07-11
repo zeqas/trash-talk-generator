b4ca0215044c713b6505dededc57673908c4e95a
zeqas <casperi52400@gmail.com>
2021-07-11 21:28:56 +0800

    feat: default textarea renewed

--- models/talkShit.js
+++ models/talkShit.js
@@ -6,7 +6,7 @@ function sample(array) {
 }
 
 function talkShitGenerator(target) {
-  if (!target) { return '身為一個使用者，點選一個對象，很容易吧?' }
+  if (!target) { return `身為一個使用者，點選一個對象，${sample(list.phrase)}吧!` }
   return `身為一個${list.job[target].title}，${sample(list.task[target])}，${sample(list.phrase)}吧！`
 }
 
